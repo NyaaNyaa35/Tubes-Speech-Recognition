083e7b81ea35387988998961633ebd0c40892b89
HansNotFound <if-19036@students.ithb.ac.id>
2022-04-21 23:26:11 +0700

    Add Feature Extraction MFCC

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 import speech_recognition as sr
 import os
+from scipy.io import wavfile
+import wavio as wv
 
 app = Flask(__name__)
 @app.route("/")
@@ -18,18 +20,29 @@ def process():
     if request.method == 'POST':
         save_path = os.path.join("", "audio.wav")
         request.files['audio_data'].save(save_path)
-    
-        with sr.AudioFile('audio.wav') as source:
+
+        wa = wv.read("audio.wav")
+        wv.write("audio_output.wav", wa.data, wa.rate,sampwidth=wa.sampwidth)
+        
+        freq_sample, sig_audio = wavfile.read("audio_output.wav")
+
+        print('\nShape of Signal:', sig_audio.shape)
+        print('Signal Datatype:', sig_audio.dtype)
+        print('Signal duration:', round(sig_audio.shape[0] / float(freq_sample), 2), 'seconds')
+        print(len(sig_audio))
+
+        add_response = "<br> Shape of Signal : " + str(sig_audio.shape) + "<br> Signal Datatype : " + str(sig_audio.dtype) + "<br> Signal duration : " + str(round(sig_audio.shape[0] / float(freq_sample), 2)) +  'seconds'
+
+        with sr.AudioFile('audio_output.wav') as source:
             audio = r.record(source, offset=None)
             try:
                 response = r.recognize_google(audio)
                 print("Text: "+response)
+                final_response = str(response) + str(add_response)
             except Exception as e:
                 print("Exception: "+str(e))
                 
-    return response
-
-    
+    return final_response
 
 if __name__ == "__main__":
     app.run(debug=True)
